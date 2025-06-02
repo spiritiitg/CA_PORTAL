7c9cbc2e7a6b1656a0fb932c267c210aafe3c146
Avinash Gupta <guptaavinash302@gmail.com>
2025-06-02 22:32:10 +0530

    contstant and docker

---
+++ backend/shared/constants.js
@@ -0,0 +1,16 @@
+export const entryResourceProperties = [
+  "name",
+  "rollNumber",
+  "outlookEmail",
+  "phoneNumber",
+  "program",
+  "branch",
+  "hostel",
+  "roomNumber",
+  "destination",
+  "outTime",
+  "inTime",
+  "isClosed",
+];
+
+export const adminResourceProperties = ["email", "password", "roles"];
